230b142cd3487965762b10d56c6ff291604e5fb8
imtiaz <imtiazahmed026@gmail.com>
2022-04-12 15:53:48 +0600

    fixing design in nuxt frontend

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,33 +1,66 @@
 <template>
-    <div>
-        <div class="_1main_content">
-            <div class="_layout">
-                <div class="_layout_row">
-                   <h1 class="text-center" style="margin-top:150px;">Hello {{authUser.first_name}} {{authUser.last_name}}!</h1>
-                </div>
-            </div>
-        </div>
-    </div>
+	<div>
+		<div class="_1main_content">
+			<div class="_layout">
+				<div class="_layout_row">
+					<!-- Sidebar -->
+					<div class="_layout_auto _layout_sidebar_left">
+						<div class="_sidebar_main">
+							<leftSidebar />
+						</div>
+					</div>
+					<!-- Sidebar -->
+
+					<!-- Feed and Status box -->
+					<div class="_layout_col">
+						<!-- Status box -->
+						<div class="_statusBox_layout">
+							<statusBox />
+						</div>
+						<!-- Status box -->
+
+						<!-- Feed -->
+						<div class="_feed_main">
+							<Feed />
+						</div>
+						<!-- Feed -->
+					</div>
+					<!-- Feed and Status box -->
+
+					<!-- Right section -->
+					<div class="_layout_auto _layout_chatList_right">
+						<div class="_rightSec_main">
+							<rightSection />
+						</div>
+					</div>
+					<!-- Right section -->
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-
+import leftSidebar from "../components/leftSidebar.vue";
+import statusBox from "../components/statusBox.vue";
+import Feed from "../components/feed.vue";
+import rightSection from "../components/rightSection.vue";
 
 export default {
-  middleware:'auth',
-  data(){
-    return{
-      
-    }
-  },
+	middleware: "auth",
+	components: {
+		leftSidebar,
+		Feed,
+		rightSection,
+		statusBox,
+	},
 
+	data() {
+		return {};
+	},
 
-  methods:{
-    
-  },
-  
-  created(){
-    
-  }
-}
+	methods: {},
+
+	created() {},
+};
 </script>
